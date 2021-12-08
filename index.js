4bd65bfc54a2a0f9ffafbbc262697c994983338f
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-12-08 12:49:19 +0000

    Criando lista js.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const precosLivros = [25, 15, 30, 50 ,45, 20];
+const livros = require('./listalivros');
 
-let atual = 0 ;
-let maisBarato = 0; 
+let maisBarato = 0;
 
-for(let atual = 0; atual < precosLivros.length; atual ++){
-  
+for (let atual = 0; atual < livros.length; atual++) {
+  if (livros[atual].preco < livros[maisBarato].preco) {
+    maisBarato = atual;
+  }
+
 }
+
+console.log(`O livro mais barato custa ${livros[maisBarato].preco} e o título é ${livros[maisBarato].titulo}`)
+
+/*Template strings são envolvidas por (acentos graves) (` `)*/
